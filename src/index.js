3f1ed1063c9150352901182cc128abb4dd2ed98d
osipenkoDenis <denis.osipenko@inbox.ru>
2020-10-19 11:12:29 +0300

    feat:complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+      n *=(-1)
+    }
+    let str = '' + n;
+    let arr = Array.from(str);
+    arr = arr.reverse();
+    let string = '';
+    for(let key in arr){
+      string += arr[key];
+    }
+    let num = +string;
+    return num;
 }
